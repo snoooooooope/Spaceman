ab4cae63aaeb14d469c2306e577209483436bae9
snoooooooope <bootnull@proton.me>
2024-05-05 16:59:50 -0400

    Core functionality
    Everything works as is, needs formatting and terminal implementation.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// #![allow(unused)]
+
+use sysinfo::{Disks, System};
+
+fn main() {
+    let mut systeminfo = System::new_all();
+    systeminfo.refresh_all();
+
+    let disks = Disks::new_with_refreshed_list();
+    for disk in disks.list() {
+        println!(
+            "{:?}, {:?}, {:?}",
+            disk.name(),
+            disk.available_space(),
+            disk.total_space()
+        );
+    }
+}
